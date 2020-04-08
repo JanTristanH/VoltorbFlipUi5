f25c64bb5fee5d959bf57da713bedb8857628545
Tristan Heisecke <tristan.heisecke@gmail.com>
2020-04-08 09:57:35 +0000

    working counter for rows

--- webapp/controller/Matchfield.controller.js
+++ webapp/controller/Matchfield.controller.js
@@ -6,9 +6,10 @@ sap.ui.define([
 	"sap/m/MessageToast",
 	"sap/base/Log",
 	"sap/ui/model/Filter",
-	"sap/ui/model/FilterOperator"
+	"sap/ui/model/FilterOperator",
+	"sap/ui/model/json/JSONModel"
 
-], function (Controller, MessageToast, Log, Filter, FilterOperator) {
+], function (Controller, MessageToast, Log, Filter, FilterOperator, JSONModel) {
 	"use strict";
 
 	return Controller.extend("ZTHE.ZTHE_VOLTORB_FLIP.controller.matchfield", {
@@ -21,9 +22,48 @@ sap.ui.define([
 
 		onInit: function () {
 			let oLayout = this.getView().byId("BlockLayout");
+			var oModel = new JSONModel();
+			this.getView().setModel(oModel, "points");
 
 			this._populateField(this, oLayout);
+			this._loadTrapAndPointCount(this);
 			//todo preload missing Images on the way out
+		},
+		_populateTextCard: (x, y, oVBox) => {
+			//todo bookmark
+			oVBox.addItem(new sap.m.Text("x" + x + "y" + y + "startext").bindText({
+				path: "points>/RowsPoints" + y
+			}));
+
+			oVBox.addItem(new sap.m.Text("x" + x + "y" + y + "firetext").bindText({
+				path: "points>/RowsTraps" + y
+			}));
+		},
+		_loadTrapAndPointCount: (that) => {
+			let oModel = that.getOwnerComponent().getModel("odata");
+			let oPointsModel = that.getView().getModel("points");
+			let loadRow = (sPath, emoji, index) => {
+				let mParam = {
+					success: function (oData) {
+						let obj = {};
+						obj[`${sPath.slice(1)}${index}`] = emoji + ": " + oData.results[0].value;
+						oPointsModel.setData(obj, true);
+					},
+					error: (oError) => {
+						Log.error(JSON.stringify(oError));
+					},
+					filters: [new Filter("y", FilterOperator.EQ, index + 1)]
+				};
+
+				oModel.read(sPath, mParam);
+
+			};
+			const sPathRowsPoints = "/RowsPoints";
+			const sPathRowsTraps = "/RowsTraps";
+			for (let y = 0; y < 5; y++) {
+				loadRow(sPathRowsPoints, starEmoji, y);
+				loadRow(sPathRowsTraps, fireEmoji, y);
+			}
 		},
 		_imageHandler: function (oEvent) {
 			//infoger
@@ -56,6 +96,7 @@ sap.ui.define([
 			oImageFrom.getParent().addItem(oImageTo);
 			oImageFrom.destroy();
 		},
+
 		_loadValueAtPosition: (that, x, y) => {
 			return new Promise((resolve, reject) => {
 				// get model
@@ -143,9 +184,9 @@ sap.ui.define([
 						oHBox.addItem(oVBox);
 					}
 					//add counter card to the end of the row
-					let oVBox = new sap.m.VBox("y" + j + "x" + 6).setWidth("100%");
+					let oVBox = new sap.m.VBox("y" + j + "x" + 6).setWidth("100%").setAlignItems("Start");
 					//todo these numbers could be bound
-					oVBox.addItem(new sap.m.Text().setText(starEmoji + ":" + "\n" + fireEmoji + ":")).setAlignItems("Start");
+					that._populateTextCard(6, j, oVBox);
 
 					oHBox.addItem(oVBox);
 					oLayout.addItem(oHBox);
