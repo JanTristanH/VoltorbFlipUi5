1847c3712e90efddc2808a539121df5b5888f58c
Tristan Heisecke <tristan.heisecke@gmail.com>
2020-07-03 05:49:00 +0000

    clean up

--- webapp/controller/Matchfield.controller.js
+++ webapp/controller/Matchfield.controller.js
@@ -13,12 +13,6 @@ sap.ui.define([
 
 	return Controller.extend("ZTHE.ZTHE_VOLTORB_FLIP.controller.matchfield", {
 
-		/**
-		 * Called when a controller is instantiated and its View controls (if available) are already created.
-		 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
-		 * @memberOf ZTHE.ZTHE_VOLTORB_FLIP.view.matchfield
-		 */
-
 		onInit: function () {
 			let oLayout = this.getView().byId("BlockLayout");
 			var oModel = new JSONModel();
@@ -28,13 +22,14 @@ sap.ui.define([
 			this._loadTrapAndPointCount(this);
 			// preload missing Images on the way out
 			setTimeout(() => {
-
 				for (let i = 0; i < 4; i++) {
 					this._loadPicture(this, i).then(d => Log.info("preload"));
 				}
 			}, 0);
 		},
 		_populateTextCard: (x, y, oVBox) => {
+			//check whether the box is located on the side or bottom
+			//ad this for counts the score of a Row or a column
 			let infix = x > y ? "Rows" : "Columns";
 
 			oVBox.addItem(new sap.m.Text("x" + x + "y" + y + "startext").bindText({
@@ -53,7 +48,6 @@ sap.ui.define([
 				let mParam = {
 					success: function (oData) {
 						oPointsModel.setProperty(sPath + index, emoji + ": " + oData.results[0].value);
-						debugger;
 					},
 					error: (oError) => {
 						Log.error(JSON.stringify(oError));
@@ -87,7 +81,7 @@ sap.ui.define([
 			let that = oView.getController(); //for private Method
 			let x = parseInt(this.getId().charAt(11), 10) + 1;
 			let y = parseInt(this.getId().charAt(9), 10) + 1;
-			//nested Promise bad
+			//nested Promise is not good but await gives syntax errors
 			that._loadValueAtPosition(oView, x, y).then(oData => {
 				//Log.debug("value for this card" + oData.value);
 				that._loadPicture(that, oData.value).then(oDataP => {
@@ -97,10 +91,9 @@ sap.ui.define([
 						mode: "Background",
 						height: "87px",
 						width: "87px"
-							//,press: that._imageHandler
 					});
 					that._flipFromToImage(oImage, oImageNew);
-					//	oImage.setSrc("data:image/png;base64," + oDataP.Picture);
+
 				});
 			});
 
@@ -110,7 +103,6 @@ sap.ui.define([
 			oImageFrom.getParent().addItem(oImageTo);
 			oImageFrom.destroy();
 		},
-
 		_loadValueAtPosition: (that, x, y) => {
 			return new Promise((resolve, reject) => {
 				// get model
@@ -127,11 +119,7 @@ sap.ui.define([
 						Log.error(JSON.stringify(oError));
 						reject(oError);
 					},
-					/*urlParameters: new Map([
-						['x', x],
-						['y', y]
-					])
-*/
+
 					filters: [new Filter("x", FilterOperator.EQ, x), new Filter("y", FilterOperator.EQ, y)]
 				};
 
@@ -144,7 +132,7 @@ sap.ui.define([
 		_loadPicture: (that, id) => {
 			return new Promise((resolve, reject) => {
 				let sPath = "/Pictures";
-				// get model
+
 				var oModel = that.getOwnerComponent().getModel("odata");
 
 				let lCache = oModel.getProperty(sPath + `(${id})`);
@@ -163,9 +151,6 @@ sap.ui.define([
 							Log.error(JSON.stringify(oError));
 							reject(oError);
 						},
-						// urlParameters: new Map([
-						// 	['id', id]
-						// ])
 						filters: [new Filter("PictureID", FilterOperator.EQ, id)]
 
 					};
@@ -215,43 +200,7 @@ sap.ui.define([
 				oHBox.addItem(oVBox);
 				oLayout.addItem(oHBox);
 			});
-		},
-
-		onShowHello: function () {
-			// read msg from i18n model
-			//var oBundle = this.getView().getModel("i18n").getResourceBundle();
-			//var sRecipient = this.getView().getModel().getProperty("/recipient/name");
-			//var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-			// show message
-			MessageToast.show("Aloh");
 		}
-
-		/**
-		 * Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
-		 * (NOT before the first rendering! onInit() is used for that one!).
-		 * @memberOf ZTHE.ZTHE_VOLTORB_FLIP.view.matchfield
-		 */
-		//	onBeforeRendering: function() {
-		//
-		//	},
-
-		/**
-		 * Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
-		 * This hook is the same one that SAPUI5 controls get after being rendered.
-		 * @memberOf ZTHE.ZTHE_VOLTORB_FLIP.view.matchfield
-		 */
-		/*		onAfterRendering: function () {
-
-				}*/
-
-		/**
-		 * Called when the Controller is destroyed. Use this one to free resources and finalize activities.
-		 * @memberOf ZTHE.ZTHE_VOLTORB_FLIP.view.matchfield
-		 */
-		//	onExit: function() {
-		//
-		//	}
-
+		
 	});
-
 });
